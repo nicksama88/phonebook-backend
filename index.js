2c8aa533f630a487ec4f1c7022c94a08d77b82f8
Nicksama88 <Nicksama88@gmail.com>
2021-01-21 16:02:58 -0800

    add delete functionality with mongoDB and error handling

--- index.js
+++ index.js
@@ -11,116 +11,110 @@ app.use(express.json())
 app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
   }
-  
-const unknownEndpoint = (request, response) => {
-response.status(404).send({ error: 'unknown endpoint' })
-}
   
 app.use(requestLogger)
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 // landing page
 app.get('/', (request, response) => {
-    response.send('<h1>Welcome to the phonebook!</h1>')
+  response.send('<h1>Welcome to the phonebook!</h1>')
 })
 
 // return full persons list
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
 })
 
 // return general info
 app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
     const len = persons.length
     response.send(
-        `<p>Phonebook has info for ${len} people</p>` + 
-        `<p>${new Date()}</p>`
+      `<p>Phonebook has info for ${len} people</p>` + 
+      `<p>${new Date()}</p>`
     )
+  })
 })
 
 // return data for specific user based on ID
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 })
 
 // deleting one person from book
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      respond.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // function for generating random new Id
 const generateId = () => {
-    // const largestId = persons.length > 0
-    //     ? Math.max(...persons.map(n => n.id))
-    //     : 0
-    // return largestId + 1
-    return Math.floor(Math.random() * Math.floor(1000))
+  return Math.floor(Math.random() * Math.floor(1000))
 }
 
 // adding new names to phone book with random phone number
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'body of request missing name or number'
-        })
-    }
-
-    if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
-        return response.status(400).json({
-            error: `'${body.name}' already in book`
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'body of request missing name or number'
     })
+  }
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({
+      error: `'${body.name}' already in book`
     })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+  
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
